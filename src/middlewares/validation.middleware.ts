40d2ea2469a49f5f149abbf18a1001b0f29ee836
rahul-b-p <rahulzantoz10@gmail.com>
2025-01-08 12:48:52 +0530

    Validation Middleware setup

---
+++ src/middlewares/validation.middleware.ts
@@ -0,0 +1,43 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError, ZodSchema } from "zod";
+import { BadRequestError } from "../errors";
+import { InternalServerError } from "../errors";
+import { logger } from "../utils";
+
+
+
+
+export const validateReqBody = (schema: ZodSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            req.body = schema.parse(req.body);
+            next();
+        } catch (error) {
+            if (error instanceof ZodError) {
+                error.errors.map((e) => {
+                    logger.error(error)
+                    return next(new BadRequestError(`Bad Request, ${e.message}`));
+                })
+            }
+            else next(new InternalServerError('Validation failed'));
+        }
+    };
+}
+
+export const validateReqQuery= (schema: ZodSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            req.query = schema.parse(req.query);
+            next();
+        } catch (error) {
+            if (error instanceof ZodError) {
+                error.errors.map((e) => {
+                    return next(new BadRequestError(`Bad Request, Invalid Query`));
+                })
+            }
+            else next(new InternalServerError('Validation failed'));
+        }
+    };
+}
+
+
